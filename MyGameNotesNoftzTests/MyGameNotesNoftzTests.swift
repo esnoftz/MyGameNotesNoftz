709c6a2f37d231b880fd5a20aaadadf1730135a2
esnoftz <esnoftz@gmail.com>
2025-02-13 07:32:30 -0600

    Initial Commit

---
+++ MyGameNotesNoftzTests/MyGameNotesNoftzTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyGameNotesNoftzTests.swift
+//  MyGameNotesNoftzTests
+//
+//  Created by EVANGELINE NOFTZ on 2/13/25.
+//
+
+import Testing
+@testable import MyGameNotesNoftz
+
+struct MyGameNotesNoftzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
